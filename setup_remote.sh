efd9f13f98a4355e41099b200f9847673227120c
remi.germe <remi.germe@gmail.com>
2023-12-23 15:16:12 +0100

    github setup for remote machine

---
+++ setup_remote.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+if [ ! -d ~/.ssh ]; then
+    mkdir ~/.ssh
+fi
+cd ~/.ssh
+
+read -p "Nom de la clé (laisser vide pour 'key_psc') : " key_name
+if ["$key_name" = ""]; then
+    key_name="key_psc"
+fi
+
+ssh-keygen -t rsa -b 4096 -N "" -f $key_name -q
+echo "Clé créée : ~/.ssh/$key_name"
+
+content=$"
+Host github.com
+  HostName github.com
+  User git
+  IdentityFile ~/.ssh/$key_name
+"
+
+echo "$content" >> ~/.ssh/config_test
+echo "Fichier config modifié"
+
+read -p "Github username : " gh_user
+read -p "Github email : " gh_email
+git config --global --add user.name $gh_user
+git config --global --add user.email $gh_email
+echo "Github configuré. Ajoutez votre clé publique sur Github :"
+echo < ~/.ssh/$key_name.pub
