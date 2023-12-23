d70def4c4347a16fa91c705dd92106b71ed80fcc
remi.germe <remi.germe@gmail.com>
2023-12-23 14:41:46 +0100

    ssh setup for Linux and macos (TODO Windows)

---
+++ setup.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+if [ ! -d ~/.ssh ]; then
+    mkdir ~/.ssh
+fi
+cd ~/.ssh
+
+read -p "Nom de votre machine de salle info (laisser vide pour 'oncidium') : " host
+if ["$host" = ""]; then
+    host="oncidium"
+fi
+
+read -p "Nom de la clé (laisser vide pour 'key_psc') : " key_name
+if ["$key_name" = ""]; then
+    key_name="key_psc"
+fi
+
+ssh-keygen -t rsa -b 4096 -N "" -f $key_name -q
+echo "Clé créée : ~/.ssh/$key_name"
+
+read -p "Nom d'utilisateur (prenom.nom) : " username
+content=$"
+Host x
+  HostName $host.polytechnique.fr
+  User $username
+  IdentityFile ~/.ssh/$key_name
+"
+
+echo "$content" >> ~/.ssh/config_test
+echo "Fichier config modifié"
+
+read -sp "Mot de passe LDAP X : " password
+echo ""
+if ! command -v sshpass &> /dev/null; then
+    if [ "$(uname)" = "Darwin" ]; then
+        brew install hudochenkov/sshpass/sshpass
+    elif [ "$(uname)" = "Linux" ]; then
+        apt-get update && apt-get upgrade && apt-get install sshpass
+    else
+        echo "Windows not supported yet."
+    fi
+fi
+sshpass -p $password ssh-copy-id -i ~/.ssh/$key_name.pub -o StrictHostKeyChecking=no $username@$host.polytechnique.fr
+echo "Clé ajoutée sur le serveur"
+
+echo "Essayez maintenant de vous connecter en tapant 'ssh x'"
